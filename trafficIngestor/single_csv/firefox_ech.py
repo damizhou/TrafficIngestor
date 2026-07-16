a775268a1f3ab3f5334b650dd088b04b2326f7f2
pcz <598069751@qq.com>
2026-07-16 09:37:15 +0800

    新增 Firefox 单 CSV 采集配置文件

--- trafficIngestor/single_csv/firefox_ech.py
+++ trafficIngestor/single_csv/firefox_ech.py
@@ -1,4 +1,4 @@
-"""禁用 ML-KEM 的 Firefox 单 CSV 采集配置。"""
+"""Firefox 单 CSV 采集配置。"""
 
 import os
 
@@ -7,8 +7,7 @@ from single_csv._common import PROJECT_ROOT
 
 CONFIG = {
     "DELETE_CSV_RECORD_ON_SUCCESS": True,
-    "CONTAINER_COUNT": 15 * 20,
-    "BASE_DST": "/netdisk/mlj/20260626/temp_disableML-KEM",
+    "BASE_DST": "/netdisk/test/firefox",
     "DOCKER_IMAGE": "chuanzhoupan/trace_spider_firefox:152.0.6",
     "BROWSER_NAME": "firefox",
     "BROWSER_VERSION_COMMANDS": (("firefox", "--version"),),
@@ -16,8 +15,8 @@ CONFIG = {
         PROJECT_ROOT,
         "scripts",
         "result",
-        "urls_tls13_hybrid_top200_disable.csv",
+        "ech_top10.csv",
     ),
 }
-RUNTIME_NAME = "traffic_capture_single_csv_firefox_disable"
-ACTION_PROFILE = "tools/browsers/firefox_disable.py"
+RUNTIME_NAME = "traffic_capture_single_csv_firefox"
+ACTION_PROFILE = "tools/browsers/firefox_ech.py"
